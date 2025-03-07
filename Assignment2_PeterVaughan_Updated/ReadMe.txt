5528c744e45e705f59d656ded6c2794e8afc1ff2
Peter Vaughan <160742536+cadalac-don@users.noreply.github.com>
2025-03-07 11:58:48 -0400

    updated - correct

---
+++ Assignment2_PeterVaughan_Updated/ReadMe.txt
@@ -0,0 +1,9 @@
+This programs goes through various node info and tasks. I do add some node info at the beginning portion of the code.
+
+I show the following capabilities of insertion: at the beginning, at the end, at the given location in the sorted list
+
+I show the following capabilities of deletion: of first node, of last node, of given item index from sorted list
+
+I can split a list, sort it and add it together. If original list is odd, add one additional number to the first half of the list.
+
+I often demonstrate my code a few times and in no particular order.
---
+++ Assignment2_PeterVaughan_Updated/untitled/main.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include "List.cpp"
+using namespace std;
+
+int main()
+{
+    List list;
+    string a;
+
+    //prepend - insert at the beginning
+    list.prepend(13);
+    cout << list.first->data << endl << list.last->data << endl <<endl;
+
+    //append - insert at the end
+    list.append(78);
+    list.append(5);
+    cout << "Updated Info:" << endl;
+    list.printInfo();
+    cout << endl;
+
+    //add value at given index
+    cout << "Size before insert: " << list.size() << endl;
+    int data = 12, pos = 2;
+    list.insertPos(pos, data);
+    cout << "Size after insert: " << list.size() << endl << endl;
+    cout << "Updated Info:" << endl;
+    list.printInfo();
+    cout << endl;
+
+    //print everything to verify all the info
+    cout<< "Print updated numbers:" << endl;
+    list.prepend(55);
+    list.prepend(99);
+    list.prepend(1);
+    list.append(50);
+    list.append(45);
+    list.printInfo();
+
+    //Print info then remove first element then print everything remaining
+    cout << endl << "Print before popping the head value:" << endl;
+    list.printInfo();
+    list.deleteHead();
+    cout << endl << "Print after deleting head value:" << endl;
+    list.printInfo();
+
+    //Print info then remove indexed element then print everything remaining
+    cout<< endl << "Print before delete index value:" << endl;
+    list.printInfo();
+    list.deleteIndexedNode(2);
+    cout<< endl << "Print after delete index value:" << endl;
+    list.printInfo();
+
+    //Print info then remove last element then print everything remaining
+    cout<< endl << "Print before the tail deletion:" << endl;
+    list.printInfo();
+    list.lastPop();
+    cout<< endl << "Print after pop:" << endl;
+    list.printInfo();
+
+
+    //splitting the node list starts
+    List** split_lists = list.splitList();
+
+    //First half as per requirement
+    cout<< endl << "Print sorted - first half:" << endl;
+    List* listA = new List();
+    listA->listCopy(split_lists[0]);
+    listA->bubbleSort();
+    listA->printInfo();
+
+    //Second half as per requirement
+    cout<< endl << "Print sorted - second half:" << endl;
+    List* listB = new List();
+    listB->listCopy(split_lists[1]);
+    listB->bubbleSort();
+    listB->printInfo();
+
+    //Merge and prints the sorted info
+    cout<< endl << "Print sorted - total:" << endl;
+    List* listC = new List();
+    listC->merge(listA, listB);
+    listC->printInfo();
+
+    return 0;
+}
